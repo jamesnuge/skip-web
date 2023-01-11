414e9f284eb57cfb6bedf6a3336bb3c82f64d691
James Nugent <james.nugent89@gmail.com>
2023-01-12 10:11:33 +1100

    Updated api to set url

--- src/components/login/loginApi.ts
+++ src/components/login/loginApi.ts
@@ -6,9 +6,11 @@ export interface LoginResponse {
     jwt: string
 }
 
+const serverUrl = process.env.SKIP_DATA_SERVER_URL || 'localhost:3000'
+
 export const loginApi = {
     login: async (request: LoginRequest) => {
-        const response = await fetch('http://localhost:3000/api/auth/login', {
+        const response = await fetch(`http://${serverUrl}/api/auth/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
